1bddb4054fa3eef62e457c8960ed2deacd0408ca
srirambhat5 <srirambhat2002@gmail.com>
2026-09-03 02:58:25 +0530

    optimized model

--- server/modules/load_vectorstore.py
+++ server/modules/load_vectorstore.py
@@ -30,7 +30,7 @@ def get_embeddings():
     from langchain_huggingface import HuggingFaceEmbeddings
 
     return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_name="sentence-transformers/paraphrase-MiniLM-L3-v2",
         model_kwargs={"device": "cpu"},
         encode_kwargs={"batch_size": 8}
     )
